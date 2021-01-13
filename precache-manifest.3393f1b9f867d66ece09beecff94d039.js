0f4ca8e2f4ece42e2f354b0db72d78b5f7d53830
yesworld <a.yesworld@gmail.com>
2021-01-13 15:03:42 +0300

    Updates

---
+++ precache-manifest.3393f1b9f867d66ece09beecff94d039.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569671787a9f1804e0c0",
+    "url": "/portfolio/css/app.54985029.css"
+  },
+  {
+    "revision": "8d5e1e1f08fb1581dcab",
+    "url": "/portfolio/css/chunk-21418ac1.2ef9e35e.css"
+  },
+  {
+    "revision": "dbe1cb6c6b5fb6f2099f",
+    "url": "/portfolio/css/chunk-vendors.f33120fe.css"
+  },
+  {
+    "revision": "aae276acb85ebf85711b",
+    "url": "/portfolio/css/donate.f9a20e24.css"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/portfolio/fonts/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/portfolio/fonts/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/portfolio/fonts/primeicons.b0f5d02f.eot"
+  },
+  {
+    "revision": "f8c89ed1d56765c5af85a143dba2b0db",
+    "url": "/portfolio/img/avatar.jpeg"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/portfolio/img/color.c7a33805.png"
+  },
+  {
+    "revision": "77d6411f677652ff885ea2feb269293f",
+    "url": "/portfolio/img/firewatch/layer_0.png"
+  },
+  {
+    "revision": "94949ef8cf7c2ac3726c3bdf00d6be53",
+    "url": "/portfolio/img/firewatch/layer_1.png"
+  },
+  {
+    "revision": "d4b1f6b37b1ec924590632317f7b8629",
+    "url": "/portfolio/img/firewatch/layer_2.png"
+  },
+  {
+    "revision": "3fba6171457e44f057a63c1776ba23a8",
+    "url": "/portfolio/img/firewatch/layer_3.png"
+  },
+  {
+    "revision": "6dac52ee2397a0aef8d7bee3ca54a8d3",
+    "url": "/portfolio/img/firewatch/layer_4.png"
+  },
+  {
+    "revision": "89b9b17c712658fe8ae0d911e80a8626",
+    "url": "/portfolio/img/firewatch/layer_5.png"
+  },
+  {
+    "revision": "58d65f50a3986f6d56ddf9be6c1f8c54",
+    "url": "/portfolio/img/firewatch/layer_6.png"
+  },
+  {
+    "revision": "a7f649cd0ed59e7a8ebd6855f5227549",
+    "url": "/portfolio/img/frameAvatar.png"
+  },
+  {
+    "revision": "e5d1653a0c3f0c0eef5b966711e056e3",
+    "url": "/portfolio/img/me.png"
+  },
+  {
+    "revision": "efbe7ee47e7720d00a0ba94a2f9418ea",
+    "url": "/portfolio/img/portfolio/d2bet.png"
+  },
+  {
+    "revision": "6608eafac473a73ea6cf75d37480a530",
+    "url": "/portfolio/img/portfolio/googleapi.png"
+  },
+  {
+    "revision": "3abda06e1c7fa1ee6f7091d622edfc3e",
+    "url": "/portfolio/img/portfolio/realrating.png"
+  },
+  {
+    "revision": "b9e78233cb2b10ea789cdba9e99a1780",
+    "url": "/portfolio/img/portfolio/reducedprinting.png"
+  },
+  {
+    "revision": "ce6f4a5474948cc1b49a676c3ca89fe8",
+    "url": "/portfolio/img/portfolio/rv.png"
+  },
+  {
+    "revision": "697dd0b1a76e93f4b2a92abeef6be7ec",
+    "url": "/portfolio/img/portfolio/vue3-plugin.png"
+  },
+  {
+    "revision": "a3ac005853a3ecd0c8ef5e8a51331e5a",
+    "url": "/portfolio/img/portfolio/wp.png"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/portfolio/img/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "28178fd3fc976224445be3aef74b4d47",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "45c5bb04066b2eb74059",
+    "url": "/portfolio/js/about.49ae6e2c.js"
+  },
+  {
+    "revision": "569671787a9f1804e0c0",
+    "url": "/portfolio/js/app.6a99eaa8.js"
+  },
+  {
+    "revision": "8d5e1e1f08fb1581dcab",
+    "url": "/portfolio/js/chunk-21418ac1.066709df.js"
+  },
+  {
+    "revision": "dbe1cb6c6b5fb6f2099f",
+    "url": "/portfolio/js/chunk-vendors.2b504d6f.js"
+  },
+  {
+    "revision": "aae276acb85ebf85711b",
+    "url": "/portfolio/js/donate.08aa0fbf.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "f906bbf7ab7d1b40c88eedf8e2ce3faf",
+    "url": "/portfolio/robots.txt"
+  }
+]);
